d505ba6ac6bb79b453a7c5ac5d59192677b08d84
sel-jazo <email@gmail.com>
2026-08-05 02:24:52 +0100

    Fixing mariadb script

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -11,14 +11,24 @@ chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 
 if [ ! -d /var/lib/mysql/mysql ]; then
-    mariadb-install-db --user=mysql  --skip-test-db
+    mariadb-install-db --user=mysql --skip-test-db --auth-root-authentication-method=normal
+fi
 
-    mariadbd  --user=mysql  --bootstrap <<EOF
+if [ ! -f /var/lib/mysql/.done ]; then
+    mariadbd --user=mysql --bootstrap <<EOF
+
+    FLUSH PRIVILEGES;
     CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
     CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+    ALTER USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
     GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
     ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
+    FLUSH PRIVILEGES;
 EOF
+
+    touch /var/lib/mysql/.done
+    chown mysql:mysql /var/lib/mysql/.done
 fi
 
 exec mariadbd
+
