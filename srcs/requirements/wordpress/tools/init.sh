1399b15a7530bb2d9d12d37fff89a999c6584941
KakitaSama <112784215+KakitaSama@users.noreply.github.com>
2026-08-06 00:48:10 +0100

    Restore WP_HOME and WP_SITEURL configuration
    Re-add setting of WP_HOME and WP_SITEURL in init.sh.

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -14,12 +14,12 @@ if [ ! -f wp-settings.php ]; then
     cp -a /usr/src/wordpress/. .
 fi
 
-wp config set WP_HOME "$WP_DYNAMIC_URL" --raw --allow-root
-wp config set WP_SITEURL "$WP_DYNAMIC_URL" --raw --allow-root
-
 if [ ! -f wp-config.php ]; then
     wp config create  --dbname="$MYSQL_DATABASE"  --dbuser="$MYSQL_USER"   --dbpass="$DB_PASSWORD"  --dbhost=mariadb --allow-root
 fi
+
+wp config set WP_HOME "$WP_DYNAMIC_URL" --raw --allow-root
+wp config set WP_SITEURL "$WP_DYNAMIC_URL" --raw --allow-root
 
 if ! wp core is-installed --allow-root; then
     wp core install  --url="https://$DOMAIN_NAME" --title="$WP_TITLE"  --admin_user="$WP_ADMIN_USER"  --admin_password="$ADMIN_PASSWORD"  --admin_email="$WP_ADMIN_EMAIL" --allow-root
